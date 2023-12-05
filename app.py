f309d3441b7a1d58b64492b5f8446f0b2b9d9cd4
ereyes187 <131736093+ereyes187@users.noreply.github.com>
2023-12-04 18:43:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -20,11 +20,18 @@ app.app_context().push() # without this, I recieve flask error
 
 
 # databases
- 
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    messages_sent = db.relationship('Message', foreign_keys='Message.sender_id', backref='sender', lazy=True)
+    messages_received = db.relationship('Message', foreign_keys='Message.receiver_id', backref='receiver', lazy=True)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,13 +41,17 @@ class Event(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('events', lazy=True))
 
-class Admin(db.Model):
+class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    content = db.Column(db.String(255), nullable=False)
+    time = db.Column(db.String(20), nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+ 
 
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Event, db.session))
+admin.add_view(ModelView(Message, db.session))
 
 @app.route('/')
 def launch():
@@ -66,7 +77,9 @@ if __name__ == '__main__':
 
         # Create a user
         new_user = User(email='john@example.com', password='password123')
+        test = User(email='test@example.com', password='abc123')
         db.session.add(new_user)
+        db.session.add(test)
         db.session.commit()
 
         # Add events for the user
@@ -74,6 +87,11 @@ if __name__ == '__main__':
         event2 = Event(date='2023-11-30', time='3:00 PM', event='Meeting with John', user=new_user)
         db.session.add(event1)
         db.session.add(event2)
+        db.session.commit()
+
+        # Add messages for the user
+        msg1 = Message(content='Hello, how are you?', time='10:00 AM', sender_id=new_user.id, receiver_id=test.id)
+        db.session.add(msg1)
         db.session.commit()
 
         # Retrieve all events for a specific user (e.g., the user with ID=1)
@@ -88,6 +106,17 @@ if __name__ == '__main__':
         print("User's Events:")
         for eventt in user.events:
             print(f"Date: {eventt.date}, Time: {eventt.time}, Event= {eventt.event}")
+
+        # Retrieve messages for the user
+        user_messages_received = Message.query.filter_by(receiver_id=user.id).all()
+        user_messages_sent = Message.query.filter_by(sender_id=user.id).all()
+        print("User's Messages:")
+        if user_messages_sent:
+            print(f"Sender: {user.email}")
+            for sent_message in user_messages_sent:
+                print(f"Message Content: {sent_message.content}, Time: {sent_message.time}")
+                print(f"Receiver: {sent_message.receiver.email}")
+
 
     app.run(debug=True)
 
@@ -115,9 +144,6 @@ if __name__ == '__main__':
 #>>> from app import app      use this to avoid error
 #>>> from app import db
 #>>> from app import User
-#>>> user1 = User(studentName="student1",email="somethin@gmail" ,password="something",role="student")
-#>>> db.session.add(user1)
-#>>> db.session.commit()
 #>>> user1 = User(studentName="student1",email="somethin@gmail" ,password="something",role="student")
 #>>> db.session.add(user1)
 #>>> db.session.commit()
